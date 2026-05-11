260848f89cd167355d728c2b4d4ab77bb0331645
成德 金 <chengde.jin@gmail.com>
2026-05-11 12:40:46 +0900

    20260511

--- OKX.Net/Objects/System/OKXNoticeSubscription.cs
+++ OKX.Net/Objects/System/OKXNoticeSubscription.cs
@@ -14,7 +14,6 @@ public class OKXNoticeSubscription : SystemSubscription
     /// <param name="OnNotice">The action to invoke when a notice message is received.</param>
     public OKXNoticeSubscription(ILogger logger, Action<OKXNotice>? OnNotice) : base(logger, false)
     {
-        base.MessageRouter = MessageRouter.CreateWithOptionalTopicFilters<OKXNotice>("notice", null, (sc, dt, st, notice) => { OnNotice?.Invoke(notice); return CallResult.SuccessResult; });
-        ;
+        base.MessageRouter = MessageRouter.CreateWithoutTopicFilter<OKXNotice>("notice", (sc, dt, st, notice) => { OnNotice?.Invoke(notice); return CallResult.SuccessResult; });
     }
 }
